1978dd0f7353c2444b2996e70d0a975b34ec5263
Gabin-ishimwe <g.ishimwe@alustudent.com>
2023-02-07 20:55:47 +0200

    fix-reset api gateway

--- api-gateway/src/main/java/com/ewallet/apiGateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ewallet/apiGateway/ApiGatewayApplication.java
@@ -3,11 +3,9 @@ package com.ewallet.apiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
